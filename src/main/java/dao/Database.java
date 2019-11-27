257ede86d4a3a5cbc47829693be881fc50c9a0a4
PavloBaluh <BaluhPavlo@gmail.comgit config --global user.email baluhpavlo@gmail.com>
2019-11-27 14:04:51 +0200

    add_database_connaction

---
+++ src/main/java/dao/Database.java
@@ -0,0 +1,23 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+  private static Connection connection;
+  private static final String URL = "jdbc:mysql://localhost:3306/bank?serverTimezone=UTC";
+  private static final String USER = "root";
+  private static final String PASSWORD = "root";
+
+  public static synchronized Connection getConnection(){
+    if (connection == null){
+      try {
+        connection = DriverManager.getConnection(URL,USER,PASSWORD);
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+    return connection;
+  }
+}
